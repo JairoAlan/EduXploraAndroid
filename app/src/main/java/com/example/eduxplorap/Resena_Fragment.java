1a285ed7db625997af6f232c42d9aacf4a8b4e3e
Jairo Alan Castelan Fernandez <jairoalanc@outlook.com>
2024-04-14 11:29:15 -0600

    Cambios minimos al Resena_Fragment.java y se comento una linea de codigo en fragment_resena_, la 110, no se que era lo que intentava Victor, pero ya..

--- app/src/main/java/com/example/eduxplorap/Resena_Fragment.java
+++ app/src/main/java/com/example/eduxplorap/Resena_Fragment.java
@@ -58,8 +58,11 @@ public class Resena_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resena_, container, false);
+        View view = inflater.inflate(R.layout.fragment_resena_, container, false);
+
+
+
+        return view;
     }
 
 
