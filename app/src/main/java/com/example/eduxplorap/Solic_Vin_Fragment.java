1aaa9b4c8fdb47a0a8f2d6c2918e9ecd8fa02523
MarianaAPeralta <2130246@upt.edu.mx>
2024-04-16 16:01:00 -0600

    Correccion de error tipográfico al mostrar los datos de las solicitudes de los docentes en las pantallas de coordinador y vinculacion asi como otros errores ortográficos.

--- app/src/main/java/com/example/eduxplorap/Solic_Vin_Fragment.java
+++ app/src/main/java/com/example/eduxplorap/Solic_Vin_Fragment.java
@@ -166,7 +166,7 @@ public class Solic_Vin_Fragment extends Fragment {
                 for(int i = 0; i < 4; i++){
                     try {
                         JSONObject objeto = new JSONObject(jsonArray.get(i).toString());
-                        textViews[i].append("Empresa: "+ objeto.getString("nombreEmpresa")+"\n");
+                        textViews[i].append("Nombre: "+ objeto.getString("nombreEmpresa")+"\n");
                         textViews[i].append("Distancia: "+"\n");
                         textViews[i].append("Url: "+ objeto.getString("Contacto")+"\n");
                         textViews[i].append("\n");
